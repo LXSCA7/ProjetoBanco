c5b8f2a2ee312e557a74f499f2783b8cee5d5230
LXSCA <lucas.edr27@gmail.com>
2024-05-26 22:17:07 -0300

    feat: Criação da classe Verificação.

---
+++ Models/Verificacao.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoBanco.Context;
+
+namespace ProjetoBanco.Models
+{
+    public class Verificacao
+    {
+        public static bool VerificaNascimento(DateTime data)
+        {
+            DateTime anoAtual = DateTime.Now;
+            if (anoAtual.Year - data.Year < 18)
+                return false;
+
+            return true;
+        }
+
+        public static bool UsuarioExiste(string nomeDeUsuario)
+        {
+            using (var db = new UsuariosContext())
+            {
+                return db.Usuarios.Any(u => u.Username == nomeDeUsuario);
+            }
+        }
+
+        public static bool UsernameAprovado(string nomeDeUsuario)
+        {
+            foreach (char c in nomeDeUsuario)
+            {
+                if (c == ' ')
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool SenhaAprovada(string senha)
+        {
+            bool caracterEspecial = false;
+            bool numeros = false;
+            bool letraMin = false;
+            bool letraMai = false;
+            if (senha.Length < 8)
+                return false;
+
+            foreach (char c in senha)
+            {
+                if (c == ' ')
+                    return false;
+
+                if (c == '!' || c == '@' || c == '#' || c == '$' || c == '%' || c == '&' || c == '.' || c == ',')
+                    caracterEspecial = true;
+                if (char.IsNumber(c))
+                    numeros = true;
+                if (char.IsUpper(c))
+                    letraMai = true;
+                if (char.IsLower(c))
+                    letraMin = true;
+            }
+
+            if (!caracterEspecial || !numeros || !letraMai || !letraMin)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}

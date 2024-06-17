edf853cd717700ccd84119c13807a694cdcf8d54
LXSCA <lucas.edr27@gmail.com>
2024-06-17 02:17:40 -0300

    tests: Adição de testes unitários para transferencia entre contas inválida.

--- BancoEstrelaTests/TestesUsuario.cs
+++ BancoEstrelaTests/TestesUsuario.cs
@@ -41,5 +41,36 @@ namespace BancoEstrelaTests
             Assert.Equal(saldoUsuario, user1.Saldo);
         }
 
+        [Fact]
+        public void NaoRealizaTransferencia_DeveRetornarErro_SaldoImpossivel()
+        {
+            var user1 = new Usuario()
+            {
+                Nome = "Joao",
+                Sobrenome = "da Silva",
+                CPF = "123.123.123-12",
+                Senha = "MyStr0ngPassword(!)",
+                Saldo = 150.00M
+            };
+
+            var user2 = new Usuario()
+            {
+                Nome = "Carlos",
+                Sobrenome = "Alberto",
+                CPF = "321.321.321-32",
+                Senha = "SenhaF0rte(!)",
+                Saldo = 2500.00M
+            };
+
+            decimal valor = 500M;
+            decimal saldoUsuario = user1.Saldo - valor;
+
+            UsuariosContext context = new();
+            UsuarioController usuarioController = new(context);
+
+            var exception = Assert.Throws<Exception>(() => usuarioController.TransferirEntreContas(user1, user2, valor));
+
+            Assert.Equal("Erro: saldo do usuario menor que valor.", exception.Message);
+        }
     }
 }

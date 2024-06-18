c5e1f4f73d63f2be02f1344902ad55afc62e4496
LXSCA <lucas.edr27@gmail.com>
2024-06-17 21:50:37 -0300

    migrations: Migração para a tabela de extrato adicionada.

---
+++ BancoEstrela/Migrations/Extrato/20240618004412_AdicionarExtrato.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoBanco.Migrations.Extrato
+{
+    /// <inheritdoc />
+    public partial class AdicionarExtrato : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sobrenome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataDeNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Saldo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PerguntaDeSeguranca = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Extratos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContaQueRealizou = table.Column<int>(type: "int", nullable: false),
+                    valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ContaQueRecebeu = table.Column<int>(type: "int", nullable: true),
+                    DataRealizada = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Extratos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Extratos_Usuario_ContaQueRealizou",
+                        column: x => x.ContaQueRealizou,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Extratos_Usuario_ContaQueRecebeu",
+                        column: x => x.ContaQueRecebeu,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Extratos_ContaQueRealizou",
+                table: "Extratos",
+                column: "ContaQueRealizou");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Extratos_ContaQueRecebeu",
+                table: "Extratos",
+                column: "ContaQueRecebeu");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Extratos");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
